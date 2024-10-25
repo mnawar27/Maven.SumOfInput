8e426ea20b2c2cdea8602e8e05d911aee3535f77
Maisha Nawar <mnawar@Zipcoders-MacBook-Pro.local>
2024-10-24 23:21:23 -0400

    finished sum and extra credit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,26 +2,68 @@
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer in Jan 2024
  */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
-        
-        int number = getInputNumber();
-        
-        int sum = sumOfNumbers(number);
-        
-        System.out.println(sum);
+
+        Main main = new Main();
+
+        int number = main.getInputNumber();
+
+       //Iteration time
+        long startTime1 = System.currentTimeMillis();
+        int sum = main.sumOfNumbers(number);
+        long endTime1 = System.currentTimeMillis();
+        long duration1 = endTime1 - startTime1;
+        System.out.println("Iteration method time: " + duration1 + " ms");
+
+
+        //Gaussian time
+        long startTime2 = System.currentTimeMillis();
+        int sum2 = main.gaussianSum(number);
+        long endTime2 = System.currentTimeMillis();
+        long duration2 = endTime2 - startTime2;
+        System.out.println("Gaussian method time: " + duration2 + " ms");
+
+        if (duration1 > duration2){
+            System.out.println("Gaussian method time is faster!");
+        }
+        else if (duration2 > duration1){
+            System.out.println("Iteration method time is faster!");
+        }
+        else{
+            System.out.println("Both methods take the same time!");
+        }
+
+        //Sum
+        System.out.println("the sum is " + sum);
 
     }
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+
+    public int sumOfNumbers(int n) {
+        int total = 0;
+        int i = 0;
+
+        while(i<=n){
+            total += i;
+            i++;
+        }
+        return total;
+    }
+
+    public int gaussianSum(int n){
+
+        int total2 =  (n*(n+1))/2;
+        return total2;
     }
     
     
-    int getInputNumber() { // gee, may thi sis useful in another lab?
+    public int getInputNumber() { // gee, may thi sis useful in another lab?
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
@@ -36,9 +78,9 @@ public class Main {
     
     
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
-    }
+    //int gaussianSumOfNumbers(int n) {
+        //return 0;
+    //}
    
     // for Extra extra credit - compare the methods and show which one is faster
     // google "how to time some java code"
